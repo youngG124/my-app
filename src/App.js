640f6b86f2fe1dcdad571147ea33d4ad63e1b032
youngG124 <newyoung124@naver.com>
2022-03-29 23:40:31 +0900

    d

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      </div>
+      <br></br>
+      <div className="App-content">
+
+      </div>
     </div>
   );
 }
